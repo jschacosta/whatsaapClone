46d43a4641a2fe6b9f44cf535e378d19d25b7979
JohannSch <jschacosta@gmail.com>
2023-08-28 15:35:31 +0200

    boton para crear chatRoom desde contactos

--- src/components/contactListItem/index.js
+++ src/components/contactListItem/index.js
@@ -1,5 +1,7 @@
 import { Text, Image, StyleSheet, Pressable, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { API,Auth, graphqlOperation } from 'aws-amplify'; 
+import { createChatroom, createUserChatroom } from '../../graphql/mutations';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -7,10 +9,37 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
 const ContactListItem = ({ user }) => {
-  const navigation = useNavigation();
+  const navigation=useNavigation()
+  onPress = async() =>{
+    console.log("press")
+    //check is already have a chatRoom with the user
+    //create a new chatRoom
+    const newChatRoomData = await  API.graphql(graphqlOperation(createChatroom,{input:{}}))
+    console.log("newChat", newChatRoomData)
+    if(!newChatRoomData.data?.createChatroom){
+      console.log("error creating chatroom")
+    }
+    const newChatRoom=newChatRoomData.data?.createChatroom
+    console.log("idChatRoom",newChatRoom.id,user.id)
+
+    // Add the clicked user to chatRrom
+    let creado=await API.graphql(graphqlOperation(createUserChatroom,{
+      input:{chatroomId:newChatRoom.id, userId:user.id}
+    }))
+    console.log("creado", creado)
+    // Add the auth to the chatRoom
+    const authUser= await Auth.currentAuthenticatedUser();
+    console.log("auth", authUser.attributes.sub)
+    await API.graphql(
+      graphqlOperation(createUserChatroom,{
+        input:{chatroomId:newChatRoom.id, userId:authUser.attributes.sub}
+      }))
+    //navigate to de newly created chatRoom
+    navigation.navigate("Chat", {id:newChatRoom.id})
+  }  
 
   return (
-    <Pressable onPress={() => {}} style={styles.container}>
+    <Pressable onPress={onPress} style={styles.container}>
       <Image source={{ uri: user.image }} style={styles.image} />
 
       <View style={styles.content}>
