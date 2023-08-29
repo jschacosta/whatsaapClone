46cc1026677d08a5ad1c1f5347833a995ee3bee5
JohannSch <jschacosta@gmail.com>
2023-08-30 00:32:41 +0200

    updatechatroom subscription chatlistitem

--- src/components/chatListItem/index.js
+++ src/components/chatListItem/index.js
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react'
 
 import { API, graphqlOperation, Auth } from 'aws-amplify'
+import { onUpdateChatroom } from '../../graphql/subscriptions';
 
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
@@ -12,6 +13,7 @@ const ChatListItem = ({chat}) =>{
     const navigation = useNavigation();
     console.log("la data", chat.users.items)
     const [user, setUser]=useState([]) 
+    const [chatRoom, setChatRoom]=useState(chat) 
 
     
     useEffect(()=>{
@@ -20,8 +22,7 @@ const ChatListItem = ({chat}) =>{
             let idUser = authUser.attributes.sub
             console.log("la iddd", idUser)
             //loop through chat.users.items and find a user is not us (Authenticaded user)
-            let user =null
-            for (let item of chat.users.items){
+            for (let item of chatRoom.users.items){
                 console.log(item)
                 console.log(item.user.id)
                 console.log(idUser)
@@ -30,10 +31,32 @@ const ChatListItem = ({chat}) =>{
             }
         }
         fetchAuth()
+
+        
       
     },[])
+
+    useEffect(() => {
+        // Subscribe to onUpdateChatRoom
+        const subscription = API.graphql(
+          graphqlOperation(onUpdateChatroom, {
+            filter: { id: { eq: chat.id } },
+          })
+        ).subscribe({
+          next: ({ value }) => {
+            setChatRoom((cr) => ({
+              ...(cr || {}),
+              ...value.data.onUpdateChatroom,
+            }));
+          },
+          error: (error) => console.warn(error),
+        });
+      
+        // Stop receiving data updates from the subscription
+        return () => subscription.unsubscribe();
+      }, [chat.id]);
     const goChat = () =>{
-        navigation.navigate('Chat',{id:chat.id,name:user.name})  
+        navigation.navigate('Chat',{id:chatRoom.id,name:user.name})  
     }
     return (
         <Pressable onPress={goChat} style={styles.container}>
@@ -44,11 +67,11 @@ const ChatListItem = ({chat}) =>{
             <View style={styles.content}>
                 <View style={styles.row}>
                     <Text  style={styles.name} numberOfLines={1}>{user?.name} </Text>
-                    {chat.LastMessage &&
-                    <Text  style={styles.subTitle}>{dayjs(chat.LastMessage?.createdAt).fromNow()}</Text>
+                    {chatRoom.LastMessage &&
+                    <Text  style={styles.subTitle}>{dayjs(chatRoom.LastMessage?.createdAt).fromNow()}</Text>
                     }
                 </View>
-                <Text  style={styles.name} numberOfLines={2}>{chat.LastMessage?.text}</Text>
+                <Text  style={styles.name} numberOfLines={2}>{chatRoom.LastMessage?.text}</Text>
             </View>
         </Pressable>
     )
